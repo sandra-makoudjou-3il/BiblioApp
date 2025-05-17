42c7d95fb0b3a829a3ce7f607a6127e235a43122
MAKOUDJOU <makoudjs@3il.fr>
2025-05-17 19:18:39 +0200

    Commit1

--- src/bibliotheque/Bibliotheque.java
+++ src/bibliotheque/Bibliotheque.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//
 import searchStrategy.ByNom;
 import searchStrategy.SearchStrategy;
 import factory.LivreFactory;
@@ -20,8 +20,8 @@ public class Bibliotheque {
 	public  List<Livre> livres ;
 	public List<Utilisateur> utilisateurs;
 	private SearchStrategy searchStrategy;
-	
-	
+
+
 	/**
 	 * @param livres
 	 * @param utilisateurs
@@ -31,7 +31,7 @@ public class Bibliotheque {
 		this.livres = new ArrayList<>();
 		this.utilisateurs = new ArrayList<>();
 	}
-	
+
 
 
 	public static Bibliotheque getInstance() {
@@ -41,7 +41,7 @@ public class Bibliotheque {
         return instance;
 	}
 
-	
+
 	public boolean addLivre(String type, String nom, String auteur, int anneePublication, float prix) {
 	    Livre livre = LivreFactory.createLivre(type, nom, auteur, anneePublication, prix);
 	    if (livre == null) {
@@ -51,63 +51,63 @@ public class Bibliotheque {
 	    return true;
 	}
 
-	
+
 	public Boolean isPresentLivre(Livre livre) {
 		return livres.contains(livre);
 	}
-	
+
 	public void removeLivre(Livre livre) {
-		
+
 		livres.remove(livre);
 	}
-	
-	
+
+
 	public void setSearchStrategy(SearchStrategy searchStrategy) {
 		this.searchStrategy= searchStrategy;
 	}
-	
-	
+
+
 	public List <Livre> applySearchStrategy(List<Livre> livres,String string) {
 		return searchStrategy.executeStrategy(livres,string);
 	}
-	
+
 	public void viewLivre() {
-		
+
 		for(Livre livre: this.livres) {
 			System.out.println(livre.toString());
 		}
 	}
-	
+
 	public void addAdherent(String nom) {
 		try {
 			utilisateurs.add(UtilisateurFactory.createUtilisateur(nom, "ADHERENT"));
 		} catch(Exception e) {
-			
+
 		}
 	}
-	
+
 	public void addAdmin(String nom) {
 		try {
 			utilisateurs.add(UtilisateurFactory.createUtilisateur(nom, "ADMIN"));
 		} catch(Exception e) {
-			
+
 		}
 	}
-	
+
 	public void removeUtilisateur(Utilisateur Utilisateur) {
-		
+
 		utilisateurs.remove(Utilisateur);
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		Bibliotheque bibliotheque=Bibliotheque.getInstance();
 		bibliotheque.addLivre("THRILLER", "a", "b",1,2);
 		bibliotheque.viewLivre();
-		
+
 	}
-	
-	
-	
+
+
+
 
 }
